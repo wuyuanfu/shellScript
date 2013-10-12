e3eaf1adb15c444701f2c1263d2b4cfcea107c55
wuyuanfu <wuyuanfu@hotmail.com>
2013-10-12 13:06:43 +0800

    增加了版本注释和多次备份确认
    添加了版本号注释，增加了多次备份时的确认

--- flowBackup.sh
+++ flowBackup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ##Create by wuyuanfu 
-##Date 2013-09-16
+##Date 2013-10-12
+##Version 20131012-1
 ## 请把脚本放在config机的asptools目录，添加执行权限。
 ## 第一次备份之前请先执行flowBackup.sh makebackuppath创建流水备份目录
+##ChangeLog
+## 20131012-1  更改流水备份目录存放在组件的安装目录的FLOWBACKUP目录,
+###			   同时增加备份完成确认标志，避免重复备份时，测试流水覆盖正式流水。
 BAKPATH=`date +%Y%m%d-%H%M%S`
 ndays=`date -d "-7 day" +%Y%m%d`
+flowpath="FLOWBACKUP/flow"
+logpath="FLOWBACKUP/log"
 rm ~/shell/BACKUPLISTTMP
 cat ~/shell/list|grep -Ev '^#' |grep -Ev '^$' >~/shell/BACKUPLISTTMP
 exec 3<~/shell/BACKUPLISTTMP
@@ -15,39 +21,79 @@ function makebackuppath(){
 		num=`echo $line|awk '{print $2}'`
 		list="${name}${num}"
 		if [ "${num}" != "csv" ];then
-			ssh ${list} "mkdir -pv ~/${list}/flow/backup/ ~/${list}/log/backup/"
-			echo -e "Backup directory of ${list} ,[flow/backup and log/backup] has been created!\n"
+			ssh ${list} "mkdir -pv ~/${list}/${flowpath} ~/${list}/${logpath}"
+			echo -e "Backup directory of ${list} ,${flowpath} and ${logpath} has been created!\n"
 		else
-			ssh ${name} "mkdir -pv ~/${name}/flow/backup/ ~/${list}/log/backup/"
-			echo -e "Backup directory of ${name} ,[flow/backup and log/backup] has been created!\n"		
+			ssh ${name} "mkdir -pv ~/${name}/${flowpath} ~/${name}/${logpath}"
+			echo -e "Backup directory of ${name} ,${flowpath} and ${logpath} has been created!\n"		
 		fi
 	done
 }
 if [ "$1" == "makebackuppath" ];then
 	makebackuppath
-	return
+	exit
 fi
-while read line <&3
-do
-	name=`echo $line|awk '{print $1}'`
-	num=`echo $line|awk '{print $2}'`
-	list="${name}${num}"
-	if [ "${num}" != "csv" ];then
-#		echo "Create the backup directory [ ~/${list}/backup/${BAKPATH} ] on ${list}" 
-#		ssh ${list} "mkdir -p ~/${list}/backup/$BAKPATH "
-		echo "Backup the flow and log files of ${list}, Please waitting..."
-		ssh ${list} "cd  ~/${list}/flow/backup && rm -f ./* && cp -vf ../* ./"
-		ssh ${list} "cd  ~/${list}/log/backup && rm -f ./* && cp -vf ../* ./"
-		echo -e "Backup ${list}'s flow and log completed"
-#		ssh ${list} "ls -lh ~/${list}/flow/backup ~/${list}/log/backup"
-#		echo "Deleting the backuped files ${ndays} ago..."
-#		ssh ${list} "find ~/${list}/backup/* -mtime +{$ndays} |xargs rm -rf -"
+if [ -e .backuped ];then
+	echo -e "You have backuped the flow and log files..."
+	echo -en 'Enter Y the backup again[y/n]:  '
+	read -n1 choose
+	if [ "${choose}" == "Y" ] || [ "${choose}" == "y" ];then
+		while read line <&3
+		do
+			name=`echo $line|awk '{print $1}'`
+			num=`echo $line|awk '{print $2}'`
+			list="${name}${num}"
+			if [ "${num}" != "csv" ];then
+				echo "Backup the flow and log files of ${list}, Please waitting..."
+##################删除历史备份文件
+				ssh ${list} "rm -f ~/${list}/${flowpath}/* ~/${list}/${logpath}/*"  
+##################备份flow目录
+				ssh ${list} "cp -vf ~/${list}/flow/* ~/${list}/${flowpath}/"
+##################备份log目录
+				ssh ${list} "cp -vf ~/${list}/log/* ~/${list}/${logpath}/"
+				echo -e "Backup ${list}'s flow and log completed"
+			else
+				echo "Backup the flow and log files of ${name}, Please waitting..."
+###################删除历史备份文件
+				ssh ${name} "rm -f ~/${name}/${flowpath}/* ~/${name}/${logpath}/*"  
+###################备份flow目录
+				ssh ${name} "cp -vf ~/${name}/flow/* ~/${name}/${flowpath}/"
+###################备份log目录
+				ssh ${name} "cp -vf ~/${name}/log/* ~/${name}/${logpath}/"
+				echo -e "Backup ${name}'s flow and log completed"		
+			fi
+			echo -e "\n"
+		done
 	else
-		echo "Backup the flow and log files of ${name}, Please waitting..."
-		ssh ${name} "cd  ~/${name}/flow/backup && rm -f ./* && cp -vf ../* ./"
-		ssh ${name} "cd  ~/${name}/log/backup && rm -f ./* && cp -vf ../* ./"
-		echo -e "Backup ${name}'s flow and log completed"		
+		exit
 	fi
-	echo -e "\n"
-done
+else
+	while read line <&3
+	do
+		name=`echo $line|awk '{print $1}'`
+		num=`echo $line|awk '{print $2}'`
+		list="${name}${num}"
+		if [ "${num}" != "csv" ];then
+			echo "Backup the flow and log files of ${list}, Please waitting..."
+###############删除历史备份文件
+			ssh ${list} "rm -f ~/${list}/${flowpath}/* ~/${list}/${logpath}/*"  
+###############备份flow目录
+			ssh ${list} "cp -vf ~/${list}/flow/* ~/${list}/${flowpath}/"
+###############备份log目录
+			ssh ${list} "cp -vf ~/${list}/log/* ~/${list}/${logpath}/"
+			echo -e "Backup ${list}'s flow and log completed"
+		else
+			echo "Backup the flow and log files of ${name}, Please waitting..."
+###############删除历史备份文件
+			ssh ${name} "rm -f ~/${name}/${flowpath}/* ~/${name}/${logpath}/*"  
+###############备份flow目录
+			ssh ${name} "cp -vf ~/${name}/flow/* ~/${name}/${flowpath}/"
+###############备份log目录
+			ssh ${name} "cp -vf ~/${name}/log/* ~/${name}/${logpath}/"
+			echo -e "Backup ${name}'s flow and log completed"		
+		fi
+		echo -e "\n"
+	done
+fi
+touch .backuped
 exec 3>&- 
--- flowRestore.sh
+++ flowRestore.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 ##Create by wuyuanfu
 ##Date 2013-09-16
+##Version 20131012-1
+##ChangLog
+#### 20131012-1  更改流水备份目录存放在组件的安装目录的FLOWBACKUP目录
 BAKPATH=`date +%Y%m%d-%H%M%S`
 PWD=`cat ~/shell/sh.cfg|grep passwd|cut -d= -f2`
 ndays=`date -d "-7 day" +%Y%m%d`
+flowpath="FLOWBACKUP/flow"
+logpath="FLOWBACKUP/log"
 rm ~/shell/BACKUPLISTTMP
 cat ~/shell/list|grep -Ev '^#' |grep -Ev '^$' >~/shell/BACKUPLISTTMP
 exec 3<~/shell/BACKUPLISTTMP
@@ -14,14 +19,17 @@ function makebackuppath(){
 		num=`echo $line|awk '{print $2}'`
 		list="${name}${num}"
 		if [ "${num}" != "csv" ];then
-			ssh ${list} "mkdir -pv ~/${list}/flow/backup/ ~/${list}/log/backup/"
-			echo -e "Backup directory [flow/backup and log/backup] has been created!\n"
+			ssh ${list} "mkdir -pv ~/${list}/${flowpath} ~/${list}/${logpath}"
+			echo -e "Backup directory of ${list} ,[flow/backup and log/backup] has been created!\n"
+		else
+			ssh ${name} "mkdir -pv ~/${name}/${flowpath} ~/${name}/${logpath}"
+			echo -e "Backup directory of ${name} ,[flow/backup and log/backup] has been created!\n"		
 		fi
 	done
 }
 if [ "$1" == "makebackuppath" ];then
 	makebackuppath
-	return
+	exit
 fi
 if [ $PWD != "" ]
 then
@@ -31,7 +39,7 @@ then
 	stty echo
 	if [ "$inpwd" = "$PWD" ]
 	then
-###   调用ecall.sh清理流水
+########调用ecall.sh清理流水
 		ecall.sh clean
 		while read line <&3
 		do
@@ -39,23 +47,23 @@ then
 			num=`echo $line|awk '{print $2}'`
 			list="${name}${num}"
 			if [ "${num}" != "csv" ];then
-	#				echo "Create the backup directory [ $(pwd) ] on ${list}" 
-	#				ssh ${list} "mkdir -p ~/${list}/backup/$BAKPATH "
-				echo "Restore the flow and log files of [ ${lsit} ],Please waitting..."
-				ssh ${list} "cd  ~/${list}/flow/ && cp -vf ./backup/* ./"
-				ssh ${list} "cd  ~/${list}/log/ && cp -vf ./backup/* ./"
+				echo "Restore the flow and log files of  ${lsit} ,Please waitting..."
+#################恢复flow流水文件
+				ssh ${list} "cp -vf ~/${list}/${flowpath}/* ~/${list}/flow"
+#################恢复log日志文件				
+				ssh ${list} "cp -vf ~/${list}/${logpath}/* ~/${list}/log"
 				echo -e "Restore ${list}'s flow and log completed"
-	#				ssh ${list} "ls -lh ~/${list}/flow/backup ~/${list}/log/backup"
-	#				echo "Deleting the backuped files ${ndays} ago..."
-	#				ssh ${list} "find ~/${list}/backup/* -mtime +{$ndays} |xargs rm -rf -"
 			else
-				echo "Restore the flow and log filesof [ ${name} ],Please waitting..."
-				ssh ${name} "cd  ~/${name}/flow/ && cp -vf ./backup/* ./"
-				ssh ${name} "cd  ~/${name}/log/ && cp -vf ./backup/* ./"
+				echo "Restore the flow and log filesof  ${name} ,Please waitting..."
+#################恢复flow流水文件
+				ssh ${name} "cp -vf ~/${name}/${flowpath}/* ~/${name}/flow"
+#################恢复log日志文件				
+				ssh ${name} "cp -vf ~/${name}/${logpath}/* ~/${name}/log"
 				echo -e "Restore ${name}'s flow and log completed"
 			fi
 			echo -e "\n"
 		done
+		rm -f .backuped
 		exec 3>&- 
 	else
 			echo "Invalid password!!!"
