78aeb271026c114aab3f38eefef21d7af1e0869d
wuyuanfu <wuyuanfu@hotmail.com>
2014-12-23 14:58:42 +0800

    Linux服务关闭脚本

---
+++ serviceStop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+logfile="tee -a serviceStop.log"
+echo "Stop the service may not need" >${logfile}
+for i in `cat servicelist`
+do
+	chkconfig --list $i 2>&1| ${logfile}
+	echo -n "Are you sure Stop this Servece[y/n]: "
+	read -s ch
+	echo
+	if [ $ch == 'Y' ] || [ $ch == 'y' ]
+	then
+	   chkconfig $i off 2>&1|${logfile}
+	else
+	   echo "You cancel the STOP action!"
+	fi
+	echo -e "_________________________________________\n"
+done
+	
