9912fc7f4535f8d3d8fac1deca3dc405b98437c8
wuyuanfu <wuyuanfu@hotmail.com>
2014-12-24 15:20:53 +0800

    Update dbbackup.sh
    新增功能，可同时备份多个数据库。

--- dbbackup.sh
+++ dbbackup.sh
@@ -4,10 +4,14 @@ filename=mysql-$(date +%Y%m%d).dmp
 username=esunnytap9
 password=Esunny123456
 MYSQL=$(which mysqldump)
+###配置数据库备份目录与数据库名，多个数据库以空格隔开
 backuplist="/home/esunny/data:TapDataBase"
-backupdir=`echo ${backuplist} |awk -F: '{print $1}'`
-dbname=`echo ${backuplist} |awk -F: '{print $2}'`
-${MYSQL} -u ${username} -p${password} --database ${dbname} > ${backupdir}/${filename}||exit 5
-if [ "$?" == "0" ];then
-	echo $(date) -- Backup is complete and file is located in ${backupdir}>>${backupdir}/dbbackup.log 2>&1;
-fi
+for i in ${backuplist}
+do
+	backupdir=`echo ${i} |awk -F: '{print $1}'`
+	dbname=`echo ${i} |awk -F: '{print $2}'`
+	${MYSQL} -u ${username} -p${password} --database ${dbname} > ${backupdir}/${filename}||exit 5
+	if [ "$?" == "0" ];then
+		echo $(date) -- Backup is complete and file is located in ${backupdir}>>${backupdir}/dbbackup.log 2>&1;
+	fi
+done
