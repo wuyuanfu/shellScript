1427bbbf987e45d2f0bb760b6b24ef9241180f2f
wuyuanfu <wuyuanfu@hotmail.com>
2020-12-07 18:50:29 +0800

    Add files via upload

--- flowBackup.sh
+++ flowBackup.sh
@@ -7,10 +7,14 @@
 ##ChangeLog
 ## 20131012-1  更改流水备份目录存放在组件的安装目录的FLOWBACKUP目录,
 ###			   同时增加备份完成确认标志，避免重复备份时，测试流水覆盖正式流水。
+###20190426-1  新增大商所bin目录dat文件备份
+###20201207-1  修改流水MD5校验方式
+
 BAKPATH=`date +%Y%m%d-%H%M%S`
 ndays=7
 DATE=`date +%Y%m%d`
 BACKUPPATH=FLOWBACKUP
+BASEPATH=`getcfg.sh BasePath`
 flowpath="FLOWBACKUP/flow"
 logpath="FLOWBACKUP/log"
 rm ~/shell/BACKUPLISTTMP
@@ -23,22 +27,23 @@ function makebackuppath(){
 		num=`echo $line|awk '{print $2}'`
 		list="${name}${num}"
 		if [ "${num}" != "csv" ];then
-			ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}"
-			echo -e "Backup directory of ${list} ,~/${list}/${BACKUPPATH} has been created!\n"
+			ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}"
+			echo -e "Backup directory of ${list} ,${BASEPATH%*/}/${list}/${BACKUPPATH} has been created!\n"
 		else
-			ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}"
-			echo -e "Backup directory of ${name} ,~/${name}/${BACKUPPATH} has been created!\n"		
+			ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}"
+			echo -e "Backup directory of ${name} ,${BASEPATH%*/}/${name}/${BACKUPPATH} has been created!\n"		
 		fi
 	done
+  echo "function makebackuppath:$BAKPATH" >>${BASEPATH%*/}/asptools/log/flowBackup.log
 }
 function fileCheck(){
-	for file in $(ssh $1 "ls ~/$1/$2/$3/$4/")
+	for file in $(ssh $1 "ls ${BASEPATH%*/}/$1/$2/$3/$4/")
 	do
-		md5A=$(ssh $1 "md5sum ~/$1/$2/$3/$4/$file |awk '{print $1}'")
-		md5B=$(ssh $1 "md5sum ~/$1/$4/$file |awk '{print $1}'")
+		md5A=$(ssh $1 "md5sum ${BASEPATH%*/}/$1/$2/$3/$4/$file" |awk '{print $1}')
+		md5B=$(ssh $1 "md5sum ${BASEPATH%*/}/$1/$4/$file" |awk '{print $1}')
 		if [ "$md5A" != "$md5B" ]
 		then
-			echo ---ERROR: MD5SUM of ~/$1/$4/$file is diff.
+			echo ---ERROR: MD5SUM of ${BASEPATH%*/}/$1/$4/$file is diff.
 			exit 110
 		fi
 	done
@@ -61,31 +66,38 @@ if [ -e .backuped_${DATE} ];then
 			if [ "${num}" != "csv" ];then
 				echo "Backup the flow and log files of ${list}, Please waitting..."
 ##################删除历史备份文件
-############### ssh ${list} "rm -rf ~/${list}/${flowpath}/* ~/${list}/${logpath}/*"  
-				ssh ${list} "find ~/${list}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
+############### ssh ${list} "rm -rf ${BASEPATH%*/}/${list}/${flowpath}/* ${BASEPATH%*/}/${list}/${logpath}/*"  
+				ssh ${list} "find ${BASEPATH%*/}/${list}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
 ##################备份flow目录
-				ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}/${BAKPATH}/flow"
-				ssh ${list} "cp -vp ~/${list}/flow/* ~/${list}/${BACKUPPATH}/${BAKPATH}/flow/"	
+				ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/flow"
+				ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/flow/* ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/flow/"	
 				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} flow				
 ##################备份log目录
-				ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}/${BAKPATH}/log"
-				ssh ${list} "cp -vp ~/${list}/log/* ~/${list}/${BACKUPPATH}/${BAKPATH}/log"
+				ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/log"
+				ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/log/* ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/log"
 				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} log
-				echo -e "Backup ${list}\'s flow and log completed"
+				echo -e "Backup ${list}\'s flow and log has completed"
 			else
 				echo "Backup the flow and log files of ${name}, Please waitting..."
 ##################删除历史备份文件
-############### ssh ${name} "rm -rf ~/${name}/${flowpath}/* ~/${name}/${logpath}/*"  
-				ssh ${name} "find ~/${name}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
+############### ssh ${name} "rm -rf ${BASEPATH%*/}/${name}/${flowpath}/* ${BASEPATH%*/}/${name}/${logpath}/*"  
+				ssh ${name} "find ${BASEPATH%*/}/${name}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
 ##################备份flow目录
-				ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}/${BAKPATH}/flow"
-				ssh ${name} "cp -vp ~/${name}/flow/* ~/${name}/${BACKUPPATH}/${BAKPATH}/flow/"
+				ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/flow"
+				ssh ${name} "cp -vp ${BASEPATH%*/}/${name}/flow/* ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/flow/"
 				fileCheck ${name} ${BACKUPPATH} ${BAKPATH} flow				
 ##################备份log目录
-				ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}/${BAKPATH}/log"
-				ssh ${name} "cp -vp ~/${name}/log/* ~/${name}/${BACKUPPATH}/${BAKPATH}/log"
+				ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/log"
+				ssh ${name} "cp -vp ${BASEPATH%*/}/${name}/log/* ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/log"
 				fileCheck ${name} ${BACKUPPATH} ${BAKPATH} log
-				echo -e "Backup ${name}'s flow and log completed"		
+				echo -e "Backup ${name}'s flow and log has completed"		
+			fi
+####增加大商所bin目录流水备份
+			if [ "${name}" == "dceoffer" ] || [ "${name}" == "dcemdserver" ];then
+				ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/bin"
+				ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/bin/*dat ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/bin/"	
+				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} bin
+				echo -e "Backup ${list}\'s bin has completed"
 			fi
 			echo -e "\n"
 		done >>~/asptools/log/flowBackup_${DATE}
@@ -101,34 +113,42 @@ else
 		if [ "${num}" != "csv" ];then
 			echo "Backup the flow and log files of ${list}, Please waitting..."
 ##################删除历史备份文件
-############### ssh ${list} "rm -rf ~/${list}/${flowpath}/* ~/${list}/${logpath}/*"  
-			ssh ${list} "find ~/${list}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
+############### ssh ${list} "rm -rf ${BASEPATH%*/}/${list}/${flowpath}/* ${BASEPATH%*/}/${list}/${logpath}/*"  
+			ssh ${list} "find ${BASEPATH%*/}/${list}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
 ##################备份flow目录
-			ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}/${BAKPATH}/flow"
-			ssh ${list} "cp -vp ~/${list}/flow/* ~/${list}/${BACKUPPATH}/${BAKPATH}/flow/"
+			ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/flow"
+			ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/flow/* ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/flow/"
 			fileCheck ${list} ${BACKUPPATH} ${BAKPATH} flow			
 ##################备份log目录
-			ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}/${BAKPATH}/log"
-			ssh ${list} "cp -vp ~/${list}/log/* ~/${list}/${BACKUPPATH}/${BAKPATH}/log"
+			ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/log"
+			ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/log/* ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/log"
 			fileCheck ${list} ${BACKUPPATH} ${BAKPATH} log
 			echo -e "Backup ${list}\'s flow and log completed"
 		else
 			echo "Backup the flow and log files of ${name}, Please waitting..."
 ##################删除历史备份文件
-############### ssh ${name} "rm -rf ~/${name}/${flowpath}/* ~/${name}/${logpath}/*"  
-			ssh ${name} "find ~/${name}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
+############### ssh ${name} "rm -rf ${BASEPATH%*/}/${name}/${flowpath}/* ${BASEPATH%*/}/${name}/${logpath}/*"  
+			ssh ${name} "find ${BASEPATH%*/}/${name}/${BACKUPPATH}/* -mtime +${ndays} -type d |xargs rm -rf" 
 ##################备份flow目录
-			ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}/${BAKPATH}/flow"
-			ssh ${name} "cp -vp ~/${name}/flow/* ~/${name}/${BACKUPPATH}/${BAKPATH}/flow/"
+			ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/flow"
+			ssh ${name} "cp -vp ${BASEPATH%*/}/${name}/flow/* ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/flow/"
 			fileCheck ${name} ${BACKUPPATH} ${BAKPATH} flow			
 ##################备份log目录
-			ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}/${BAKPATH}/log"
-			ssh ${name} "cp -vp ~/${name}/log/* ~/${name}/${BACKUPPATH}/${BAKPATH}/log"
+			ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/log"
+			ssh ${name} "cp -vp ${BASEPATH%*/}/${name}/log/* ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/log"
 			fileCheck ${name} ${BACKUPPATH} ${BAKPATH} log
 			echo -e "Backup ${name}'s flow and log completed"		
 		fi
+####增加大商所bin目录流水备份
+		if [ "${name}" == "dceoffer" ] || [ "${name}" == "dcemdserver" ];then
+			ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/bin"
+			ssh ${list} "cp -vp ${BASEPATH%*/}/${list}/bin/*dat ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/bin/"	
+			fileCheck ${list} ${BACKUPPATH} ${BAKPATH} bin
+			echo -e "Backup ${list}\'s bin has completed"
+		fi		
 		echo -e "\n"
 	done >>~/asptools/log/flowBackup_${DATE}
 fi
 touch .backuped_${DATE}
+echo "flowBackup at :${BAKPATH}">>~/asptools/log/flowBackup.log
 exec 3>&- 
--- flowRestore.sh
+++ flowRestore.sh
@@ -4,6 +4,8 @@
 ##Version 20131012-1
 ##ChangLog
 #### 20131012-1  更改流水备份目录存放在组件的安装目录的FLOWBACKUP目录
+#### 20190426-1  新增dce报盘bin目录的流水恢复
+#### 20201207-1  修改MD5校验方式
 
 PWD=`cat ~/shell/sh.cfg|grep passwd|cut -d= -f2`
 if [ "x$1" == "x" ];then
@@ -14,6 +16,7 @@ BAKPATH="$1"
 ndays=7
 DATE=`date +%Y%m%d`
 BACKUPPATH=FLOWBACKUP
+BASEPATH=`getcfg.sh BasePath`
 flowpath="FLOWBACKUP/flow"
 logpath="FLOWBACKUP/log"
 rm ~/shell/BACKUPLISTTMP
@@ -26,11 +29,11 @@ function makebackuppath(){
 		num=`echo $line|awk '{print $2}'`
 		list="${name}${num}"
 		if [ "${num}" != "csv" ];then
-			ssh ${list} "mkdir -pv ~/${list}/${BACKUPPATH}"
-			echo -e "Backup directory of ${list} ,~/${list}/${BACKUPPATH} has been created!\n"
+			ssh ${list} "mkdir -pv ${BASEPATH%*/}/${list}/${BACKUPPATH}"
+			echo -e "Backup directory of ${list} ,${BASEPATH%*/}/${list}/${BACKUPPATH} has been created!\n"
 		else
-			ssh ${name} "mkdir -pv ~/${name}/${BACKUPPATH}"
-			echo -e "Backup directory of ${name} ,~/${name}/${BACKUPPATH} has been created!\n"		
+			ssh ${name} "mkdir -pv ${BASEPATH%*/}/${name}/${BACKUPPATH}"
+			echo -e "Backup directory of ${name} ,${BASEPATH%*/}/${name}/${BACKUPPATH} has been created!\n"		
 		fi
 	done
 }
@@ -39,13 +42,13 @@ if [ "$1" == "makebackuppath" ];then
 	exit
 fi
 function fileCheck(){
-	for file in $(ssh $1 "ls  ~/$1/$2/$3/$4/")
+	for file in $(ssh $1 "ls  ${BASEPATH%*/}/$1/$2/$3/$4/")
 	do
-		md5A=$(ssh $1 "md5sum ~/$1/$2/$3/$4/$file |awk '{print $1}'")
-		md5B=$(ssh $1 "md5sum ~/$1/$4/$file |awk '{print $1}'")
+		md5A=$(ssh $1 "md5sum ${BASEPATH%*/}/$1/$2/$3/$4/$file |awk '{print $1}'")
+		md5B=$(ssh $1 "md5sum ${BASEPATH%*/}/$1/$4/$file |awk '{print $1}'")
 		if [ "$md5A" != "$md5B" ]
 		then
-			echo ---ERROR: MD5SUM of ~/$1/$4/$file is diff.
+			echo ---ERROR: MD5SUM of ${BASEPATH%*/}/$1/$4/$file is diff.
 			exit 110
 		fi
 	done
@@ -68,22 +71,28 @@ then
 			if [ "${num}" != "csv" ];then
 				echo "Restore the flow and log files of  ${list} ,Please waitting..."
 #################恢复flow流水文件
-				ssh ${list} "cp -vpf ~/${list}/${BACKUPPATH}/${BAKPATH}/flow/* ~/${list}/flow"
+				ssh ${list} "cp -vpf ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/flow/* ${BASEPATH%*/}/${list}/flow"
 				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} flow
 #################恢复log日志文件				
-				ssh ${list} "cp -vpf ~/${list}/${BACKUPPATH}/${BAKPATH}/log/* ~/${list}/log"
+				ssh ${list} "cp -vpf ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/log/* ${BASEPATH%*/}/${list}/log"
 				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} log				
 				echo -e "Restore ${list}'s flow and log completed"
 			else
 				echo "Restore the flow and log files of  ${name} ,Please waitting..."
 #################恢复flow流水文件
-				ssh ${name} "cp -vpf ~/${name}/${BACKUPPATH}/${BAKPATH}/flow/* ~/${name}/flow"
+				ssh ${name} "cp -vpf ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/flow/* ${BASEPATH%*/}/${name}/flow"
 				fileCheck ${name} ${BACKUPPATH} ${BAKPATH} flow				
 #################恢复log日志文件				
-				ssh ${name} "cp -vpf ~/${name}/${BACKUPPATH}/${BAKPATH}/log/* ~/${name}/log"
+				ssh ${name} "cp -vpf ${BASEPATH%*/}/${name}/${BACKUPPATH}/${BAKPATH}/log/* ${BASEPATH%*/}/${name}/log"
 				fileCheck ${name} ${BACKUPPATH} ${BAKPATH} log				
-				echo -e "Restore ${name}\'s flow and log completed"
+				echo -e "Restore ${name}\'s flow and log completed"				
 			fi
+####增加大商所bin目录流水恢复
+			if [ "${name}" == "dceoffer" ] || [ "${name}" == "dcemdserver" ];then
+				ssh ${list} "cp -vpf ${BASEPATH%*/}/${list}/${BACKUPPATH}/${BAKPATH}/bin/* ${BASEPATH%*/}/${list}/bin/ "	
+				fileCheck ${list} ${BACKUPPATH} ${BAKPATH} bin
+				echo -e "Restore ${list}\'s bin has completed"
+			fi			
 			echo -e "\n"
 		done >>~/asptools/log/flowRestore_${DATE}
 		rm -f .backuped_${DATE}
